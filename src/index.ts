99689b363825f551ea017bb27bdd7c552b31d2b8
Dmitriy Kozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2018-10-15 17:44:27 +0300

    Add env variables, show current app version

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import * as http from 'http';
+import * as dotenv from 'dotenv';
+
+dotenv.load();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+console.log(process.env.VERSION);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(`Current app version: ${process.env.VERSION}\n`);
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
