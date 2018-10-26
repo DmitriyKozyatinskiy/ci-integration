8d3fc904cc135c0b5910ef85f66a405c6ffc28d9
dkmv <nbvchjost@gmail.com>
2018-10-26 19:09:27 +0300

    Add mock test

--- src/__tests__/requests.test.ts
+++ src/__tests__/requests.test.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   getCurrentESTTime,
   getCurrentCETTime,
+  getCurrentUTCTime,
 } from '../requests';
 import {
   URL_EST_TIME,
@@ -44,4 +45,20 @@ it('CET time request should response with the correct time', async () => {
   expect(axios.get).toHaveBeenCalledTimes(1);
   expect(axios.get).toHaveBeenCalledWith(URL_CET_TIME);
   expect(time).toEqual('Current CET time: 2018-10-26T17:15+02:00');
+});
+it('UTC time request should response with the correct time', async () => {
+  const response = {
+    data: {
+      currentDateTime: '2018-10-26T16:04Z',
+    },
+  };
+
+  // @ts-ignore
+  axios.get.mockResolvedValue(response);
+
+  const time = await getCurrentUTCTime();
+
+  expect(axios.get).toHaveBeenCalledTimes(1);
+  expect(axios.get).toHaveBeenCalledWith(URL_UTC_TIME);
+  expect(time).toEqual('Current UTC time: 2018-10-26T16:04Z');
 });
